cd4d975227838c7bfa229b1bfa0ccf24316e6320
ibrahimparvez2 <31848160+ibrahimparvez2@users.noreply.github.com>
2018-02-04 21:49:13 +0000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using QuestionApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+     
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new QViewModel());
+        }
+
+
+        [HttpPost]
+        public JsonResult GetAnswer(string json)
+        {
+            
+            int index = _rnd.Next(_db.Count);
+            var answer = _db[index];
+
+            return Json(answer);
+
+        }
+
+        private static Random _rnd = new Random();
+        private static List<string> _db = new List<string> { "yes", "no", "Definitely yes", "I don't know", "Looks Like yes" };
+    }
+}
---
+++ Models/QViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuestionApp.Models
+{
+    public class QViewModel
+    {
+        //[Required]
+        //[MinLength(10)]
+        //[MaxLength(100)]
+        [Display(Name = "Ask for magic 8 ball any question?")]
+         public string Question { get; set; }
+
+         public string Answer { get; set; }
+
+        //public QViewModel(string question) {
+        //    this.Question = question;
+        //}
+
+    }
+}
